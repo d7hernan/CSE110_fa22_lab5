a6d9ff8373608a0129dab593300ddfc3d4928fc7
d7hernan <83631798+d7hernan@users.noreply.github.com>
2022-10-30 01:47:44 -0700

    made some progress

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  // setting image and audio file
+
+  const select = document.getElementById('horn-select');
+  select.addEventListener('change', horn_change); 
+  
+  function horn_change() {
+      const image = document.querySelector('img');
+      const sound = document.querySelector('audio');
+      //console.log(image);
+      if (select.value == "air-horn") {
+          image.src = "assets/images/air-horn.svg"; 
+          sound.src = "assets/audio/air-horn.mp3";
+      }
+      if (select.value == "car-horn") { 
+          image.src = "assets/images/car-horn.svg"; 
+          sound.src = "assets/audio/car-horn.mp3";
+      }
+      if (select.value == "party-horn") { 
+          image.src = "assets/images/party-horn.svg"; 
+          sound.src = "assets/audio/party-horn.mp3";
+      }
+      //console.log(image);
+      //console.log(sound);
+    }
+
+  const button = document.querySelector('button');
+  console.log(button);
+  button.addEventListener('click', play); 
+
+  function play() {
+    const controls = document.querySelector('div');
+    console.log(controls); 
+    const sound = document.querySelector('audio');
+    //button.controls = controls; 
+    console.log(sound); 
+    sound.play(); 
+  }
 }
